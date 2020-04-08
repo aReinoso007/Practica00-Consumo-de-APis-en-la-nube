507be8129557ce1e3b1723b8d923742aa0a5d7ee
Alex Reinoso <ralexjessiel@gmail.com>
2020-04-08 15:41:15 -0500

    css - centrar tablas
    agregar estilos para la pagina, falta arreglar un par de cosas

--- Consumo de Apis/scripts.js
+++ Consumo de Apis/scripts.js
@@ -21,13 +21,15 @@ function buscarPorTitulo(){
         var data = JSON.parse(this.responseText)
         data.Search.forEach(movie => {
           detalles += "<tr>"+
-                      "<td><a href='#' onclick=\"buscarPorID{'"+movie.imdbID + "')\">"+
+                      
                       "<td><img src="+movie.Poster + "></td" +
                       "<td></td>"+
                       "<td>" + movie.Title + "</td>" +
                       "<td>" + movie.Year + "</td>" +
                       "<td>" + movie.Type + "</td>" +
-                      "</tr>";                      
+                      "<td><a href='#'  style='text-decoration:none'     onclick=\"buscarPorID('" + movie.imdbID + "')\">'<i class='fa fa-eye'></i>'</a>" +
+                      "</tr>";          
+                                  
         });
         document.getElementById("informacion").innerHTML = detalles;
       }
@@ -39,43 +41,3 @@ function buscarPorTitulo(){
 }
 
 
-/*
-
-function movieSearch(q){
-  $.get("https://www.omdbapi.com/?s="+q+"&apikey=70833c90", function(rawdata){
-  
-  var rawString = JSON.stringify(rawdata);
-  data = JSON.parse(rawString);
-  for(var i=0; i < data.length;i++){
-
-  }
-  var titulo = data.Search[0].Title;
-  var anio = data.Search[0].Year;
-  var url = "https://www.imdb.com/title/"+data.Search[0].imdbID+"/";
-
-  var poster = data.Search[0].Poster;
-    
-  document.getElementById('answer').innerHTML="<h1>"+titulo+"</h1><br> <img src= '"+poster+"'><br><p> Year Released:"+anio+"</p> <br> <p> IMDB page: <a href='"+url+"'target='_blank'>"+url+"</a></p>"; 
-  });
-}
-function apiCall() {
-  
-  //$. para correr JQueries
-  var randNumb = Math.floor((Math.random() * randMArray.length - 1) + 1 );
-  var randMovie = randMArray[randNumb];
-
-  $.getJSON('https://www.omdbapi.com/?t=' + encodeURI(randMovie)+'&apikey=70833c90').then(function(response){
-    
-    var image = response.Poster;
-    if(image !== 'N/A'){
-      //si el request retorna falso a la respuesta N/A entonces se reemplaza la foto de 'no image' por la que se obtiene de la respuesta
-      $('img').attr('src', image)
-    }
-
-  });
-  //.then es una promesa, es algo que se haga una vez se ha obtenido los datos, 
-}
-
-//obtiene la variable 'button'
-apiCall();
-*/
